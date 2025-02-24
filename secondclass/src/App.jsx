497b1979f5d08590f158133da9347788cd9c7ac5
manishkumarsingh2302 <manishkumarbkj2005@gmail.com>
2025-02-24 19:03:03 +0530

    succes

--- secondclass/src/App.jsx
+++ secondclass/src/App.jsx
@@ -1,11 +1,23 @@
 
-
+import myimg from './photo.png'
 function App() {
-  
-
+  let name="manish"
+ 
   return (
-    <h1>welcome to React class</h1>
-    
+  //  <>
+/* <img src={myimg} alt="" />
+
+<h1>my name is {name}</h1>
+</> */
+
+  <div>
+  <ul>
+  <li>Home</li>
+  <li>contact</li>
+  <li>about</li>
+  </ul>
+  </div>
+
   )
 }
 
